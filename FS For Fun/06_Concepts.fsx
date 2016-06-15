783738a16564a3288f7ad11e7bff4f41f9bade14
Jacob Malter <jdmalter@ncsu.edu>
2016-06-14 21:23:14 -0400

    Completed a few more articles and learned some stuff.

---
+++ FS For Fun/06_Concepts.fsx
@@ -0,0 +1,70 @@
+﻿let square x = x * x
+
+// functions as values
+let squareclone = square
+let result = [1..10] |> List.map squareclone
+
+// functions taking other functions as parameters
+let execFunction aFunc aParam = aFunc aParam
+let result2 = execFunction square 12
+
+// ======== Product Types ========
+// declare it
+type IntAndBool = {intPart: int; boolPart: bool}
+
+// use it
+let x = {intPart=1; boolPart=false}
+
+// ======== Sum Types ========
+// declare it
+type IntOrBool =
+    | IntChoice of int
+    | BoolChoice of bool
+
+// use it
+let y = IntChoice 42
+let z = BoolChoice true
+
+// ======== Pattern Matching ========
+// replacement of if-then-else
+let ifThenElse booleanExpression =
+    match booleanExpression with
+        | true -> true // true branch
+        | false -> false // false branch
+
+// replacement of switch
+let switch aDigit =
+    match aDigit with
+        | 1 -> 1 // Case when digit=1
+        | 2 -> 2 // Case when digit=2
+        | _ -> 0 // Case otherwise
+
+// replacement of loops
+let loop aList =
+    match aList with
+        | [] -> [] // Empty case
+        | first::rest -> first::rest // Case with at least one element.  Process first element,
+                     // and then call recursively with the rest of the list
+
+type Shape = // define a union of alternative structures
+    | Circle of int
+    | Rectangle of int * int
+    | Polygon of (int * int) list
+    | Point of (int * int)
+
+let draw shape = // define a function "draw" with a shape parameter
+    match shape with
+    | Circle radius ->
+        printfn "The circle has a radius of %d" radius
+    | Rectangle (height,width) ->
+        printfn "The rectangle is %d high and %d wide" height width
+    | Polygon points ->
+        printfn "The polygon is made of these points %A" points
+    | _ -> printfn "I don't recognize this shape"
+
+let circle = Circle(10)
+let rect = Rectangle(4,5)
+let polygon = Polygon( [(1,1); (2,2); (3,3)] )
+let point = Point(2,3)
+
+[circle; rect; polygon; point] |> List.iter draw
---
+++ FS For Fun/10_Boilerplate.fsx
@@ -0,0 +1,57 @@
+﻿let product n =
+    let initialValue = 1
+    let action productSoFar x = productSoFar * x
+    [1..n] |> List.fold action initialValue
+
+// test
+product 10
+
+let sumOfOdds n =
+    let initialValue = 0
+    let action sumSoFar x = if x % 2 = 0 then sumSoFar else sumSoFar + x
+    [1..n] |> List.fold action initialValue
+
+// test
+sumOfOdds 10
+
+let alternatingSum n =
+    let initialValue = (true,0)
+    let action (isNeg,sumSoFar) x = if isNeg then (false,sumSoFar-x)
+                                             else (true ,sumSoFar+x)
+    [1..n] |> List.fold action initialValue |> snd
+
+// test
+alternatingSum 100
+
+let sumOfSquaresWithFold n =
+    let initialValue = 0
+    let action sumSoFar x = sumSoFar + (x * x)
+    [1..n] |> List.fold action initialValue
+
+// test
+sumOfSquaresWithFold 100
+
+type NameAndSize = {Name:string;Size:int}
+
+let maxNameAndSize list =
+    let innerMaxNameAndSize initialValue rest =
+        let action maxSoFar x = if maxSoFar.Size < x.Size then x else maxSoFar
+        rest |> List.fold action initialValue
+
+    // handle empty lists
+    match list with
+    | [] ->
+        None
+    | first::rest ->
+        let max = innerMaxNameAndSize first rest
+        Some max
+
+// test
+let list = [
+    {Name="Alice"; Size=10}
+    {Name="Bob"; Size=1}
+    {Name="Carol"; Size=12}
+    {Name="David"; Size=5}
+    ]
+maxNameAndSize list
+maxNameAndSize []
