ff181324c38186affbe48f156a6ae8e65c3d93fc
Jacob Malter <jdmalter@ncsu.edu>
2016-06-17 21:03:26 -0400

    Completed Convenience examples.
    I can do FizzBuzz with unique F# features (active pattern matching).

---
+++ FS For Fun/15_Interface.fsx
@@ -0,0 +1,45 @@
+﻿let add1 input = input + 1
+let times2 input = input * 2
+
+let genericLogger anyFunc input =
+    printfn "input is %A" input // Log the input
+    let result = anyFunc input // Evaluate the function
+    printfn "result is %A" result // Log the result
+    result // Return the result
+
+let add1WithLogging = genericLogger add1
+let times2WithLogging = genericLogger times2
+
+// test
+add1WithLogging 3
+times2WithLogging 3
+
+[1..5] |> List.map add1WithLogging
+
+let genericTimer anyFunc input =
+    let stopwatch = System.Diagnostics.Stopwatch()
+    stopwatch.Start()
+    let result = anyFunc input // evaluate the function
+    printfn "elapsed ms in %A" stopwatch.ElapsedMilliseconds
+    result
+
+let add1WithTimer = genericTimer add1WithLogging
+
+// test
+add1WithTimer 3
+
+// Strategy design pattern
+type Animal(noiseMakingStrategy) =
+    member this.MakeNoise =
+        noiseMakingStrategy() |> printfn "Making noise %s"
+
+// now create a cat
+let meowing() = "Meow"
+let cat = Animal(meowing)
+cat.MakeNoise
+
+// .. and a dog
+let woofOrBark() = if (System.DateTime.Now.Second % 2 = 0)
+                   then "Woof" else "Bark"
+let dog = Animal(woofOrBark)
+dog.MakeNoise // try again a second later
---
+++ FS For Fun/16_Partial.fsx
@@ -0,0 +1,44 @@
+﻿// define a adding function
+let add x y = x + y
+
+// normal use
+let z = add 1 2
+
+let add42 = add 42
+
+// use the new function
+add42 2
+add42 3
+
+let genericLogger before after anyFunc input =
+    before input // callback for custom behavior
+    let result = anyFunc input // evalute the function
+    after result // callback for custom behavior
+    result // return the result
+
+let add1 input = input + 1
+
+// reuse case 1
+genericLogger
+    (fun x -> printf "before=%i. " x) // function to call before
+    (fun x -> printfn " after=%i." x) // function to call after
+    add1 // main function
+    2 // parameter
+
+// reuse casae 2
+genericLogger
+    (fun x -> printf "started with=%i " x) // different callback
+    (fun x -> printfn " ended with=%i" x) // function to call after
+    add1 // main function
+    2 // parameter
+
+let add1WithConsoleLogging =
+    genericLogger
+        (fun x -> printf "input=%i. " x)
+        (fun x -> printfn " result=%i" x)
+        add1
+        // Last parameter NOT defined here yet
+
+// test
+add1WithConsoleLogging 2
+[1..5] |> List.map add1WithConsoleLogging
