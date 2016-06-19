52b7a63e0830273bbd630e5c61d949ca73b78af6
Jacob Malter <jdmalter@ncsu.edu>
2016-06-19 19:20:34 -0400

    Finished Completeness examples and excluded Concurrency examples because I have no need for concurrency yet.

---
+++ FS For Fun/28_Interoperation.fsx
@@ -0,0 +1,113 @@
+﻿// using an Int32
+let (i1success,i1) = System.Int32.TryParse("123")
+if i1success then printfn "Parsed as %i" i1 else printfn "parse failed"
+
+let (i2success,i2) = System.Int32.TryParse("hello")
+if i2success then printfn "Parsed as %i" i1 else printfn "parse failed"
+
+// using a DateTime
+let (d1success,d1) = System.DateTime.TryParse("1/1/1980")
+if d1success then printfn "Parsed as %A" d1 else printfn "parse failed"
+
+let (d2success,d2) = System.DateTime.TryParse("hello")
+if d2success then printfn "Parsed as %A" d2 else printfn "parse failed"
+
+// using a dictionary
+let dict = new System.Collections.Generic.Dictionary<string,string>()
+dict.Add("a","hello")
+let (e1success,e1) = dict.TryGetValue("a")
+if e1success then printfn "Parsed as %A" e1 else printfn "parse failed"
+
+let (e2success,e2) = dict.TryGetValue("b")
+if e2success then printfn "Parsed as %A" e2 else printfn "parse failed"
+
+let createReader fileName = new System.IO.StreamReader(path=fileName)
+
+let (|Digit|Letter|Whitespace|Other|) ch =
+    if System.Char.IsDigit(ch) then Digit
+    else if System.Char.IsLetter(ch) then Letter
+    else if System.Char.IsWhiteSpace(ch) then Whitespace
+    else Other
+
+let printChar ch =
+    match ch with
+    | Digit -> printfn "%c is a Digit" ch
+    | Letter -> printfn "%c is a Letter" ch
+    | Whitespace -> printfn "%c is a Whitespace" ch
+    | _ -> printfn "%c is something else" ch
+
+// print a list
+['a';'b';'1';' ';'-';'c'] |> List.iter printChar
+
+open System.Data.SqlClient
+
+let (|ConstraintException|ForeignKeyException|Other|) (ex:SqlException) =
+    if ex.Number = 2601 then ConstraintException
+    else if ex.Number = 2627 then ConstraintException
+    else if ex.Number = 547 then ForeignKeyException
+    else Other
+
+let executeNonQuery (sqlCommand:SqlCommand) =
+    try
+        let result = sqlCommand.ExecuteNonQuery()
+        result
+        // handle success
+    with
+    | :? SqlException as sqlException -> // if a SqlException
+        match sqlException with // nice pattern matching
+        | ConstraintException -> 2601 // handle constraint error
+        | ForeignKeyException -> 547 // handle FK error
+        | _ -> reraise() // don't handle any other cases
+    // all non SqlExceptions are thrown normally
+
+// create a new object that implements IDisposable
+let makeResource name = 
+    { new System.IDisposable with member this.Dispose() = printfn "%s disposed" name }
+
+let useAndDisposeResources =
+    use r1 = makeResource "first resource"
+    printfn "using first resource"
+    for i in [1..3] do
+        let resourceName = sprintf "\tinner resoruce %d" i
+        use temp = makeResource resourceName
+        printfn "\tdo something with %s" resourceName
+    use r2 = makeResource "second resource"
+    printfn "using second resource"
+    printfn "done."
+
+type IAnimal = abstract member MakeNoise : unit -> string
+
+let showTheNoiseAnAnimalMakes (animal:IAnimal) =
+    animal.MakeNoise() |> printfn "Making noise %s"
+
+type Cat = Felix | Socks
+type Dog = Butch | Lassie
+
+// now mixin the interface with the F# types
+type Cat with member this.AsAnimal = { new IAnimal with member a.MakeNoise() = "Meow" }
+type Dog with member this.AsAnimal = { new IAnimal with member a.MakeNoise() = "Woof" }
+
+let dog = Lassie
+showTheNoiseAnAnimalMakes (dog.AsAnimal)
+
+let cat = Felix
+showTheNoiseAnAnimalMakes (cat.AsAnimal)
+
+open System.Reflection
+open Microsoft.FSharp.Reflection
+
+// create a record type...
+type Account = {Id: int; Name: string}
+
+// ... and show the fields
+let fields =
+    FSharpType.GetRecordFields(typeof<Account>)
+    |> Array.map (fun propInfo -> propInfo.Name, propInfo.PropertyType.Name)
+
+// create a union type...
+type Choices = | A of int | B of string
+
+// and show the choices
+let choices =
+    FSharpType.GetUnionCases(typeof<Choices>)
+    |> Array.map (fun choiceInfo -> choiceInfo.Name)
---
+++ FS For Fun/29_Anything.fsx
@@ -0,0 +1,156 @@
+﻿// interface
+type IEnumerator<'a> =
+    abstract member Current : 'a
+    abstract MoveNext : unit -> bool
+
+// abstract base class with virtual methods
+[<AbstractClass>]
+type Shape() =
+    // readonly properties
+    abstract member Width: int with get
+    abstract member Height: int with get
+    // non-virtual method
+    member this.BoundingArea = this.Height * this.Width
+    // virtual method with base implementation
+    abstract member Print : unit -> unit
+    default this.Print () = printfn "I'm a shape"
+
+// concrete class that inherits from base class and overrides
+type Rectangle(x:int, y:int) =
+    inherit Shape()
+    override this.Width = x
+    override this.Height = y
+    override this.Print () = printfn "I'm a rectangle"
+
+// test
+let r = Rectangle(2,3)
+printfn "The width is %i" r.Width
+printfn "The area is %i" r.BoundingArea
+r.Print()
+
+type Circle(rad:int) =
+    inherit Shape()
+
+    // mutable field
+    let mutable radius = rad
+
+    // property overrides
+    override this.Width = radius * 2
+    override this.Height = radius * 2
+
+    // alternate constructor with default radius
+    new() = Circle(10)
+
+    // property with get and set
+    member this.Radius
+        with get() = radius
+        and set(value) = radius <- value
+
+// test constructors
+let c1 = Circle() // parameterless ctor
+printfn "The width is %i" c1.Width
+let c2 = Circle(2)
+printfn "The width is %i" c2.Width
+
+// test mutable property
+c2.Radius <- 3
+printfn "The width is %i" c2.Width
+
+// standard generics
+type KeyValuePair<'a,'b>(key:'a, value:'b) =
+    member this.Key = key
+    member this.Value = value
+
+// generics with constraints
+type Container<'a,'b
+    when 'a : equality
+    and 'b :> System.Collections.ICollection>
+    (name:'a,values:'b) =
+    member this.Name = name
+    member this.Values = values
+
+type Point2D =
+    struct
+        val X: float
+        val Y: float
+        new(x: float, y: float) = { X = x; Y = y }
+    end
+
+// test
+let p = Point2D() // zero initialized
+let p2 = Point2D(2.0,3.0) // explicitly initialized
+
+// create a new Exception class
+exception MyError of string
+
+try
+    let e = MyError("Oops!")
+    raise e
+with
+    | MyError msg ->
+        printfn "The exception error was %s" msg
+    | _ ->
+        printfn "Some other exception"
+
+type System.String with
+    member this.StartsWithA = this.StartsWith "A"
+
+// test
+let s = "Alice"
+printfn "'%s' starts with an 'A' = %A" s s.StartsWithA
+
+type System.Int32 with
+    member this.IsEven = this % 2 = 0
+
+// test
+let i = 20
+if i.IsEven then printfn "%i is even" i
+
+open System
+type MyConsole() =
+    member this.WriteLine([<ParamArray>] args: Object[]) =
+        for arg in args do
+            printfn "%A" arg
+
+// test
+let cons = new MyConsole()
+cons.WriteLine("abc", 42, 3.14, true)
+
+type MyButton() =
+    let clickEvent = new Event<_>()
+
+    [<CLIEvent>]
+    member this.OnClick = clickEvent.Publish
+
+    member this.TestEvent(arg) =
+        clickEvent.Trigger(this, arg)
+
+// test
+let myButton = new MyButton()
+myButton.OnClick.Add(fun (sender,arg) ->
+    printfn "Click event with arg=%O" arg)
+myButton.TestEvent("Hello World!")
+
+// delegates
+type MyDelegate = delegate of int -> int
+let f = MyDelegate (fun x -> x * x)
+let result = f.Invoke(5)
+
+// enums
+type Color = | Red=1 | Green=2 | Blue=3
+
+let color1 = Color.Red // simple assignment
+let color2:Color = enum 2 // cast from int
+// create from parsing a string
+let color3 = System.Enum.Parse(typeof<Color>,"Green") :?> Color // :?> i s downcast
+
+[<System.FlagsAttribute>]
+type FileAccess = | Read=1 | Write=2 | Execute=4
+let fileaccess = FileAccess.Read ||| FileAccess.Write
+
+open System.Windows.Forms
+
+let form = new Form(Width=400,Height=300,Visible=true,Text="Hello World")
+form.TopMost <- true
+form.Click.Add (fun args -> printfn "the form was clicked")
+form.Show()
