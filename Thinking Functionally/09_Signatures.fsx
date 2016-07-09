578a3afc2c5143cc8b68f710ef084f816f7a6f6c
Jacob Malter <jdmalter@ncsu.edu>
2016-07-09 13:43:42 -0400

    Completed two more articles on function signatures and organization.

---
+++ Thinking Functionally/09_Signatures.fsx
@@ -0,0 +1,32 @@
+﻿[1;2;3] // a normal expression
+type intlist = int list // a type expression
+
+Some 1 // a normal expression
+type intoption = int option // a type expression
+
+(1,"a") // a normal expression
+type intstring = int * string // a type expression
+
+// expression syntax // type syntax
+let add1 x = x + 1 // int -> int
+let add x y = x + y // int -> int -> int
+let print x = printf "%A" x // 'a -> unit
+System.Console.ReadLine // unit -> string
+[0] |> List.sum // 'a list -> a
+List.filter // ('a -> bool) -> 'a list -> 'a list
+List.map // ('a -> 'b) -> 'a list -> 'b list
+
+type Adder = int -> int
+type AdderGenerator = int -> Adder
+
+let a:AdderGenerator = fun x -> (fun y -> x + y)
+let c = fun (x:float) -> (fun y -> x + y)
+
+let testA x = x + x
+let testB x y = x + y
+let testC x = testB x
+let testD (x:int->int) = x 0
+let testE x y z = x + y + z
+let testF (x:int->int) = x >> (*) -1
+let testG x y = y(x + x) * y(x + x)
+let testH x = x 0 1 + x 2 3
---
+++ Thinking Functionally/10_Organizing.fsx
@@ -0,0 +1,99 @@
+﻿let addThreeNumbers x y z =
+    
+    // create a nested helper function
+    let add n =
+        fun x -> x + n
+
+    x |> add y |> add z
+
+// test
+addThreeNumbers 2 3 4
+
+let validateSize max n =
+    
+    // create a nested helper function with no params
+    let printError() =
+        printfn "Oops: '%i' is bigger than max: '%i'" n max
+
+    // use the helper function
+    if n > max then printError()
+
+// test
+validateSize 10 9
+validateSize 10 11
+
+let sumNumbersUpTo max =
+    
+    // recursive helpder function with accumulator
+    let rec recursiveSum n sumSoFar =
+        match n with
+        | 0 -> sumSoFar
+        | _ -> recursiveSum (n-1) (n+sumSoFar)
+
+    // call helper function with initial values
+    recursiveSum max 0
+
+// test
+sumNumbersUpTo 10
+
+// wtf does this function do?
+let f x = 
+    let f2 y = 
+        let f3 z = 
+            x * z
+        let f4 z = 
+            let f5 z = 
+                y * z
+            let f6 () = 
+                y * x
+            f6()
+        f4 y
+    x * f2 x
+
+// test
+[-4;-3;-2;-1;0;1;2;3;4] |> List.map f // f computes x^3
+
+[<RequireQualifiedAccess>]
+module MathStuff =
+
+    let add x y = x + y
+    let subtract x y = x - y
+
+    // nested module
+    [<RequireQualifiedAccess>]
+    module FloatLib =
+
+        let add x y :float = x + y
+        let subtract x y :float = x - y
+
+    // type definitions
+    type Complex = {r:float; i:float}
+    type IntegerFunction = int -> int -> int
+    type DegreesOrRadians = Deg | Rad
+
+    // "constant"
+    let PI = 3.141
+
+    // "variable"
+    let mutable TrigType = Deg
+
+    // initialization / static constructor
+    do printfn "module initialized"
+
+module OtherStuff =
+    // open MathStuff // make all function accessible
+    
+    // use a function from the MathStuff module
+    let add1 x = MathStuff.add x 1
+
+    // fully qualified
+    let add1Float x = MathStuff.FloatLib.add x 1.0
+
+    // with a relative path
+    let sub1Float x = MathStuff.FloatLib.subtract x 1.0
+
+// module
+module Example =
+    
+    // declare the type inside a module
+    type PersonType = {First:string; Last:string}
