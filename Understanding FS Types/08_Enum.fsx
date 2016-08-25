601df5bffc68d6d387187ca159ed7079152bc6b8
Jacob Malter <jdmalter@ncsu.edu>
2016-08-25 00:33:00 -0400

    Complete quick articles.

---
+++ Understanding FS Types/08_Enum.fsx
@@ -0,0 +1,24 @@
+﻿type SizeUnion = Small | Medium | Large // union
+type ColorEnum = Red = 0 | Yellow = 1 | Blue = 2 // enum
+
+[<System.FlagsAttribute>]
+type PermissionFlags = Read = 1 | Write = 2 | Execute = 4
+let permission = PermissionFlags.Read ||| PermissionFlags.Write
+
+let small = Small // Unions do not need to be qualified
+let red = ColorEnum.Red // Enums must be qualified
+
+let redInt = int ColorEnum.Red // cast from the underlying int type
+let redAgain:ColorEnum = enum redInt // cast to a specified enum type
+let yellowAgain = enum<ColorEnum>(1) // or create directly
+let unknownColor = enum<ColorEnum>(99) // valid
+
+let values = System.Enum.GetValues(typeof<ColorEnum>)
+let redFromString =
+    System.Enum.Parse(typeof<ColorEnum>,"Red")
+    :?> ColorEnum // downcast needed
+
+let qualifiedMatch x =
+    match x with
+    | ColorEnum.Red -> printfn "red" // qualified name used
+    | _ -> printfn "something else" // includes unknown color!
---
+++ Understanding FS Types/09_Built-in.fsx
@@ -0,0 +1,23 @@
+﻿let x = int 1.23
+let y = float 1
+let z = System.Convert.ToBoolean(0) //ok
+
+// create a function with parameter of type Object
+let objFunction (o:obj) = o
+
+// test: call with an integer
+let result = objFunction 1
+
+let o = box 1
+result = o
+
+let i:int = unbox o
+
+let detectTypeBoxed v =
+    match box v with // used "box v"
+    | :? int -> printfn "this is an int"
+    | _ -> printfn "something else"
+
+// test
+detectTypeBoxed 1
+detectTypeBoxed 3.14
